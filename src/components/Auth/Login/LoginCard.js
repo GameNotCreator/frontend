f780f868a5a24cb1d8fa82bff47b605907884606
GameNotCreator <78528763+GameNotCreator@users.noreply.github.com>
2023-06-21 20:26:16 +0100

    Update LoginCard.js

--- src/components/Auth/Login/LoginCard.js
+++ src/components/Auth/Login/LoginCard.js
@@ -29,7 +29,7 @@ function LoginCard() {
     const data = { email, password, restaurant };
 
     try {
-      const response = await axios.post('http://164.132.113.53:3500/api/auth/login', data, { withCredentials: true });
+      const response = await axios.post('https://hnh.tn/api/auth/login', data, { withCredentials: true });
 
       if (response.data.error) {
         setErrorMessage(response.data.error);
@@ -38,7 +38,7 @@ function LoginCard() {
         console.log(document.cookie); // print cookies
         setErrorMessage('') 
         // Récupérer les informations de l'utilisateur
-        fetch('http://164.132.113.53:3500/api/auth/namedisplay', {
+        fetch('https://hnh.tn/api/auth/namedisplay', {
           credentials: 'include'  // pour envoyer les cookies avec la requête
         })
           .then(response => response.json())
