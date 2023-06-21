4a8327a817213f7d710572467ac80a3d26f38490
GameNotCreator <78528763+GameNotCreator@users.noreply.github.com>
2023-06-21 20:23:51 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,7 +27,7 @@ function NavBar() {
 
 
   useEffect(() => {
-    fetch('http://164.132.113.53:3500/api/auth/namedisplay', {
+    fetch('https://hnh.tn/api/auth/namedisplay', {
       credentials: 'include'  // pour envoyer les cookies avec la requête
     })
     .then(response => response.json())
