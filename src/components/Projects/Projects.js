bd2bf37c205a443f5d1ddfbfb1b3a8aa7e939af6
GameNotCreator <78528763+GameNotCreator@users.noreply.github.com>
2023-06-21 20:24:40 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,7 +8,7 @@ function Projects() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3500/api/products') // Remplacez cette URL par l'URL de votre serveur.
+    axios.get('https://hnh.tn/api/products') // Remplacez cette URL par l'URL de votre serveur.
       .then(response => {
         setProducts(response.data);
       })
